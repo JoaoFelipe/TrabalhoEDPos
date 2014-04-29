41ae26138243d6bed462c5aac354995f6599ce55
Joao Felipe <joaofelipenp@gmail.com>
2014-04-29 12:53:21 -0300

    Estrutura de dados

---
+++ graph.h
@@ -0,0 +1,28 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdlib.h>
+
+struct edge;
+
+typedef struct node {
+	int number;
+	struct node *next;
+	struct edge *edges;
+} TNode;
+
+typedef struct edge {
+	struct node *node;
+	struct edge *next;
+} TEdge;
+
+
+TNode * new_node(int number, TNode *next);
+
+TEdge * new_edge(TNode *node, TEdge *next);
+
+void free_edges(TEdge *edge);
+
+void free_nodes(TNode *node);
+
+#endif
